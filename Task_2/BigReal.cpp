2cc789a33a3b289425b6ec22c034e87957b5c7a9
AhmedEhab2022 <am8122004@gmail.com>
2023-10-26 15:33:37 +0300

    add required files in Task 2

---
+++ Task_2/BigReal.cpp
@@ -0,0 +1,70 @@
+#include "BigReal.h"
+
+bool BigReal::isValidReal (string realNumber)
+{
+    
+}
+
+BigReal::BigReal(double realNumber = 0.0)
+{
+
+}
+
+BigReal::BigReal (string realNumber)
+{
+
+}
+
+BigReal::BigReal (const BigReal& other)
+{
+
+}
+
+BigReal& BigReal::operator = (BigReal& other)
+{
+
+}
+void BigReal::setNum (string realNumber)
+{
+
+}
+
+int BigReal::size()
+{
+
+}
+
+int BigReal::sign()
+{
+
+}
+
+BigReal BigReal::operator + (BigReal& other)
+{
+
+}
+
+BigReal operator - (BigReal& other)
+{
+
+}
+
+bool BigReal::operator < (BigReal anotherReal)
+{
+
+}
+
+bool BigReal::operator > (BigReal anotherReal)
+{
+
+}
+
+bool BigReal::operator == (BigReal anotherReal)
+{
+
+}
+
+friend ostream& BigReal::operator << (ostream& out, BigReal num)
+{
+
+}
