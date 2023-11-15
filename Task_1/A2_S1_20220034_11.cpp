6f8b3e24d28430cc8319d6b8840fc1278149256a
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2023-11-15 23:33:49 +0200

    add pdf, zip and improve prob 11

--- Task_1/A2_S1_20220034_11.cpp
+++ Task_1/A2_S1_20220034_11.cpp
@@ -63,7 +63,7 @@ int main(){
             getline(file1 , line1);
             getline(file2 , line2);
             // compare lines by each other
-            for(int i=0 ; i<line1.size() ; i++){
+            for(int i=0 ; i<max(line1.size(),line2.size() ) ; i++){
                 if(line1[i]==line2[i])
                     continue;
                 else{
@@ -86,47 +86,49 @@ int main(){
             cout<<"The files are identical";
     }
     else if(comparison == 2){
-        // counter for lines of the file
-        int lines=0;
+        // counter for lines of the files
+        int file1line=0 , file2line=0;
+        // vectors to save words of the files
+        vector<pair <string,int> > s1;
+        vector<pair <string,int> > s2;
         bool flag=true;
         // loop on the full file
-        while( file1.good() ){
-            lines++;
+        while( file1.good()){
             // get line from the file
             getline(file1 , line1);
-            //create an object ArrOfWords1 to take line1 as substrings
+            file1line++;
+            //create an object ArrOfWords1 to take line1 as words
             istringstream ArrOfWords1(line1);
             string word1;
-            vector<string> s1;
-            // take every word and save it in an index of string vector
+            // take every word and save it in an index of string vector with number of line
             while (ArrOfWords1 >> word1)
-                s1.push_back(word1);
+                s1.push_back (make_pair (word1, file1line));
+            }
+            while( file2.good()){
             getline(file2 , line2);
+            file2line++;
             istringstream ArrOfWords2(line2);
             string word2;
-            vector<string> s2;
             while (ArrOfWords2 >> word2)
-                s2.push_back(word2);
+                s2.push_back (make_pair (word2, file2line));
+            }
             // compare every word in s1 by every word in s2
             for(int i=0 ; i<s1.size() ; i++){
-                if(s1[i]==s2[i])
+                if(s1[i].first==s2[i].first)
                     continue;
                 else{
                     flag =false;
                     cout<<"The files are not identical\n";
-                    cout<<"The number of the first line that is different is : "<<lines<<'\n';
+                    cout<<"The number of the first line that is different in the first file is : "<<s1[i].second<<'\n';
+                    cout<<"The number of the first line that is different in the second file is : "<<s2[i].second<<'\n';
                     cout<<"The  first different word of the first file is :"<<'\n';
-                    cout<<s1[i]<<'\n';
+                    cout<<s1[i].first<<'\n';
                     cout<<"The  first different word of the second file is :"<<'\n';
-                    cout<<s2[i]<<'\n';
+                    cout<<s2[i].first<<'\n';
                     //break for loop
                     break;
                 }
             }
-            //break while loop
-            if(!flag)
-                break;
-        }
         if(flag)
             cout<<"The files are identical";
     }
